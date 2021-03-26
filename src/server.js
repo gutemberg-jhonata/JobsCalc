fd3d8ab6651fe6c6a2833a15be0f76077e35c3dd
Jhonata <gutemberg.jhonata@gmail.com>
2021-03-25 21:59:32 -0300

    Add public directory

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const server = express()
 
-server.listen(3000, () => console.log('rodando!'));
+server.use(express.static("public"));
 
 server.get('/', (request, response) => {
-    return response.send('Teste');
+    return response.sendFile(__dirname + "/views/index.html");
 })
+
+server.listen(3000);
