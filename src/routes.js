e1225340a7940feeb657aef20a1bda508548824e
Jhonata <gutemberg.jhonata@gmail.com>
2021-03-25 23:24:14 -0300

    Passing data to profile page

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,18 @@ const routes = express.Router();
 
 const views = __dirname + "/views/";
 
+const profile = {
+    name: "Jhonata Gutemberg",
+    avatar: "http://github.com/gutemberg-jhonata.png",
+    monthlyBudget: 3000,
+    hoursPerDay: 5,
+    daysPerWeek: 5,
+    vacationPerYear: 4
+}
+
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) => res.render(views + "job"))
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
-routes.get('/profile', (req, res) => res.render(views + "profile"))
+routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
 module.exports = routes;
