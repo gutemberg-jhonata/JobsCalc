2bdc402d9776c1dca887ef6f837768a78f0ff069
Jhonata <gutemberg.jhonata@gmail.com>
2021-03-31 15:47:20 -0300

    Implementing POST jobs route to crate a new job

--- src/routes.js
+++ src/routes.js
@@ -12,14 +12,46 @@ const profile = {
     vacationPerYear: 4
 }
 
-const jobs = [];
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        dailyHours: 2,
+        totalHours: 40,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        dailyHours: 3,
+        totalHours: 47,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+    }
+];
 
-routes.get('/', (req, res) => res.render(views + "index"))
+routes.get('/', (req, res) => {
+    return res.render(views + "index");
+})
 routes.get('/job', (req, res) => res.render(views + "job"))
+
 routes.post('/job', (req, res) => {
-    jobs.push(req.body);
+    const lastId = jobs[jobs.length - 1]?.id || 0;
+    const body = req.body;
+
+    jobs.push({
+        id: lastId + 1,
+        name: body.name,
+        dailyHours: body["daily-hours"],
+        totalHours: body["total-hours"],
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+    });
+
     return res.redirect('/');    
 })
+
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile }))
 
