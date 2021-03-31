b5795c00ae45abc4323e8f672234c66147b2789f
Jhonata <gutemberg.jhonata@gmail.com>
2021-03-31 08:46:04 -0300

    Configuring the server to enable req.body

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const routes = require("./routes");
 
 server.set("view engine", "ejs");
 server.use(express.static("public"));
+server.use(express.urlencoded({ extended: true }));
 server.use(routes);
 
 server.listen(3000);
