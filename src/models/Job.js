66b945a25437a9063d605e628c658ec64559e8aa
Jhonata <gutemberg.jhonata@gmail.com>
2021-04-02 12:38:22 -0300

    Creating model Job

---
+++ src/models/Job.js
@@ -0,0 +1,36 @@
+let data = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        dailyHours: 2,
+        totalHours: 40,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        dailyHours: 3,
+        totalHours: 3,
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+    }
+];
+
+module.exports = {
+    get() {
+        return data;
+    },
+
+    save(newJob) {
+        data.push(newJob);
+    },
+
+    update(newJobs) {
+        data = newJobs;
+    },
+
+    delete(id) {
+        data = data.filter(job => Number(job.id) !== Number(id))
+    }
+};
