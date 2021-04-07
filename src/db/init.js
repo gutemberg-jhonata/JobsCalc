b54c74a9e1f896e1212ecde4f28b51900908e0f9
Jhonata <gutemberg.jhonata@gmail.com>
2021-04-06 22:29:52 -0300

    Create profile table

---
+++ src/db/init.js
@@ -0,0 +1,17 @@
+const Database = require('config');
+
+Database()
+
+Database.exec(`
+CREATE TABLE profile (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    avatar TEXT,
+    monthly-budget INT,
+    hours-per-day INT,
+    days-per-week INT,
+    vacation-per-year INT,
+    value-hour INT,
+)`)
+
+Databasea.close()
