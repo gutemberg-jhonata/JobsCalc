1ebfbed2ace767a4d08a549a3d4de991c1563d93
Jhonata <gutemberg.jhonata@gmail.com>
2021-04-06 23:42:13 -0300

    Refactoring config script init-db

--- src/db/init.js
+++ src/db/init.js
@@ -1,79 +1,85 @@
-const Database = require('config');
+const Database = require("./config");
 
-Database()
+const initDb = {
+    async init() {
+        const db = await Database();
 
-Database.exec(`
-    CREATE TABLE profile (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        avatar TEXT,
-        monthly_budget INT,
-        hours_per_day INT,
-        days_per_week INT,
-        vacation_per_year INT,
-        value_hour INT,
-    )
-`)
+        await db.exec(`
+            CREATE TABLE profile (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                avatar TEXT,
+                monthly_budget INT,
+                hours_per_day INT,
+                days_per_week INT,
+                vacation_per_year INT,
+                value_hour INT
+            )
+        `);
 
-Database.exec(`
-    CREATE TABLE jobs (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        daily_hours INT,
-        total_hours INT,
-        created_at DATETIME,
-        updated_at DATETIME
-    )
-`)
+        await db.exec(`
+            CREATE TABLE jobs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                daily_hours INT,
+                total_hours INT,
+                created_at DATETIME,
+                updated_at DATETIME
+            )
+        `);
 
-Database.run(`
-    INSERT INTO profile (
-        name,
-        avatar, 
-        monthly_budget, 
-        hours_per_day, 
-        days_per_week, 
-        vacation_per_year
-    ) VALUES (
-        "Jhonata Gutemberg",
-        "http://github.com/gutemberg-jhonata.png",
-        3000,
-        5,
-        5,
-        4
-    )
-`)
+        await db.run(`
+            INSERT INTO profile (
+                name,
+                avatar, 
+                monthly_budget, 
+                hours_per_day, 
+                days_per_week, 
+                vacation_per_year
+            ) VALUES (
+                "Jhonata Gutemberg",
+                "http://github.com/gutemberg-jhonata.png",
+                3000,
+                5,
+                5,
+                4
+            )
+        `);
 
-Database.run(`
-    INSERT INTO jobs (
-        name,
-        daily_hours,
-        total_hours,
-        created_at,
-        updated_at
-    ) VALUES (
-        "Pizzaria Guloso",
-        2,
-        1,
-        ${Date.now()},
-        ${Date.now()}
-    )
-`)
+        await db.run(`
+            INSERT INTO jobs (
+                name,
+                daily_hours,
+                total_hours,
+                created_at,
+                updated_at
+            ) VALUES (
+                "Pizzaria Guloso",
+                2,
+                1,
+                ${Date.now()},
+                ${Date.now()}
+            )
+        `);
 
-Database.run(`
-    INSERT INTO jobs (
-        name,
-        daily_hours,
-        total_hours,
-        created_at,
-        updated_at
-    ) VALUES (
-        "OneTwo Project",
-        3,
-        47,
-        ${Date.now()},
-        ${Date.now()}
-    )
-`)
+        await db.run(`
+            INSERT INTO jobs (
+                name,
+                daily_hours,
+                total_hours,
+                created_at,
+                updated_at
+            ) VALUES (
+                "OneTwo Project",
+                3,
+                47,
+                ${Date.now()},
+                ${Date.now()}
+            )
+        `);
 
-Databasea.close()
+        await db.close();
+    }
+};
+
+initDb.init();
