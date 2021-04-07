6ce2f9d6573a754f948ea1f8805b7464d1294885
Jhonata <gutemberg.jhonata@gmail.com>
2021-04-06 22:47:28 -0300

    Inserting default profile value into profile table

--- src/db/init.js
+++ src/db/init.js
@@ -3,25 +3,45 @@ const Database = require('config');
 Database()
 
 Database.exec(`
-CREATE TABLE profile (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    avatar TEXT,
-    monthly-budget INT,
-    hours-per-day INT,
-    days-per-week INT,
-    vacation-per-year INT,
-    value-hour INT,
-)`)
+    CREATE TABLE profile (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        avatar TEXT,
+        monthly_budget INT,
+        hours_per_day INT,
+        days_per_week INT,
+        vacation_per_year INT,
+        value_hour INT,
+    )
+`)
 
 Database.exec(`
-CREATE TABLE jobs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    daily-hours INT,
-    total-hours INT,
-    created_at DATETIME,
-    updated_at DATETIME
-)`)
+    CREATE TABLE jobs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        daily_hours INT,
+        total_hours INT,
+        created_at DATETIME,
+        updated_at DATETIME
+    )
+`)
+
+Database.run(`
+    INSERT INTO profile (
+        name,
+        avatar, 
+        monthly_budget, 
+        hours_per_day, 
+        days_per_week, 
+        vacation_per_year
+    ) VALUES (
+        "Jhonata Gutemberg",
+        "http://github.com/gutemberg-jhonata.png",
+        3000,
+        5,
+        5,
+        4
+    )
+`)
 
 Databasea.close()
